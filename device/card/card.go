62f0d30c50632801e138aa7e70c5c74eccf95c86
Jacob Kaufmann <jacobkaufmann18@gmail.com>
2019-08-18 09:44:57 -0700

    Add history field to card and associated functions.

--- device/card/card.go
+++ device/card/card.go
@@ -4,11 +4,13 @@ package card
 type Card struct {
 	Question string `json:"question"`
 	Answer   string `json:"answer"`
+	History  []bool `json:"history"`
 }
 
 // New returns a new card.
 func New(q, a string) *Card {
-	return &Card{q, a}
+	var history []bool
+	return &Card{q, a, history}
 }
 
 // A Type contains templates for mapping fields to the question and answer
@@ -30,4 +32,29 @@ type Template map[string]interface{}
 // Query returns the question and answer for c.
 func (c *Card) Query() (q, a string) {
 	return c.Question, c.Answer
+}
+
+// Record appends a bool to the history of c indicating whether or not the
+// question was answered correctly.
+func (c *Card) Record(correct bool) {
+	c.History = append(c.History, correct)
+}
+
+// ClearHistory resets the performance history of c.
+func (c *Card) ClearHistory() {
+	c.History = make([]bool, 0)
+}
+
+// LastAttempt returns a bool indicating whether c was correctly answered on
+// the most recent attempt according to the history.
+func (c *Card) LastAttempt() bool {
+	if len(c.History) == 0 {
+		return false
+	}
+	return c.History[len(c.History)-1]
+}
+
+// NumAttempts returns the number of times an answer has been recorded for c.
+func (c *Card) NumAttempts() int {
+	return len(c.History)
 }
