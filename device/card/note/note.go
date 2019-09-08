8dcb482ab313a01afcb4cfc07cc7d5e526fa0e98
Jacob Kaufmann <jacobkaufmann18@gmail.com>
2019-09-07 19:55:13 -0700

    Basic Note methods and Notebook type.

--- device/card/note/note.go
+++ device/card/note/note.go
@@ -9,10 +9,31 @@ type Note struct {
 	Type    Type                   `json:"type"`
 }
 
+// New returns a new note containing content and of type noteType.
+func New(content map[string]interface{}, noteType Type) *Note {
+	return &Note{content, noteType}
+}
+
+// UpdateField updates field in n to hold value.
+func (n *Note) UpdateField(field string, value interface{}) {
+	n.Content[field] = value
+}
+
 // A Type defines a set of fields a note may have and a set of card types to
 // define the ways a note can be represented as a card.
 type Type struct {
 	Name      string          `json:"name"`
 	Fields    map[string]bool `json:"fields"`
 	CardTypes []*card.Type    `json:"cardTypes"`
+}
+
+// A Notebook is a collection of notes.
+type Notebook struct {
+	Name  string  `json:"name"`
+	Notes []*Note `json:"notes"`
+}
+
+// NewNotebook returns a new Notebook containing notes.
+func NewNotebook(name string, notes []*Note) *Notebook {
+	return &Notebook{name, notes}
 }
