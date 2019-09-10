cfbd053fd8c6b27180d05b97b70615067a342de4
Jacob Kaufmann <jacobkaufmann18@gmail.com>
2019-09-09 20:25:19 -0700

    Update UX of study cmd.

--- cmd/study.go
+++ cmd/study.go
@@ -5,7 +5,9 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/jacobkaufmann/mnemonic/device/card"
 
@@ -20,7 +22,8 @@ const (
 
 	prefixQuestion = "Question:"
 	prefixAnswer   = "Answer:"
-	suffixQuestion = "[press enter for answer]"
+	suffixQuestion = "[ENTER] for answer"
+	resultPrompt   = "Correct? [y/n]: "
 )
 
 var (
@@ -33,7 +36,7 @@ var (
 func MakeStudyCmd() *cobra.Command {
 	cmdStudy := &cobra.Command{
 		Use:   "study [type to study]",
-		Short: "study content of a given type",
+		Short: "Study content of a given type",
 	}
 
 	cmdStudy.PersistentFlags().StringP(
@@ -62,7 +65,7 @@ func cmdStudyDeck() *cobra.Command {
 
 			var deck card.Deck
 			path := filepath.Join(cli.DeckDir(), name+fileExtensionJSON)
-			err = unmarshalContentJSON(path, &deck)
+			err = readFileJSON(path, &deck)
 			if err != nil {
 				return err
 			}
@@ -76,6 +79,22 @@ func cmdStudyDeck() *cobra.Command {
 					return err
 				}
 				cli.DisplayMessage(fmt.Sprintf("%s %s\n", prefixAnswer, a))
+
+				result, err := cli.PromptUser(resultPrompt)
+				if err != nil {
+					return err
+				}
+				result = strings.TrimSpace(result)
+				if strings.ToLower(result) == "y" {
+					v.AddRecord(true)
+				} else {
+					v.AddRecord(false)
+				}
+			}
+
+			err = writeFileJSON(path, &deck, os.ModePerm)
+			if err != nil {
+				return err
 			}
 
 			return nil
@@ -85,15 +104,34 @@ func cmdStudyDeck() *cobra.Command {
 	return cmdStudy
 }
 
+// questionPrompt is a helper function to generate the prompt for a question.
 func questionPrompt(q string) string {
-	return fmt.Sprintf("%s %s %s", prefixQuestion, q, suffixQuestion)
+	return fmt.Sprintf("\n%s\n  - %s\n\n%s\n", prefixQuestion, q, suffixQuestion)
 }
 
-func unmarshalContentJSON(path string, v interface{}) error {
+// readFileJSON is a helper function to read a JSON file specified by path and
+// decode the data into v.
+func readFileJSON(path string, v interface{}) error {
 	data, err := ioutil.ReadFile(path)
 	if err != nil {
 		return err
 	}
 
 	return json.Unmarshal(data, v)
+}
+
+// writeFileJSON is a helper function to JSON encode v and write the result to
+// a file specified by path.
+func writeFileJSON(path string, v interface{}, perm os.FileMode) error {
+	data, err := json.Marshal(v)
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(path, data, perm)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
